e5a1a69d8975f03593252e786359931aa1b16e89
Mishra123456 <mishra123best@gmail.com>
2026-03-24 00:12:00 +0530

    Final commit before pushing to V1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ProtectedRoute } from "@/components/ProtectedRoute";
 import Dashboard from "./pages/Dashboard";
 import UploadPage from "./pages/UploadPage";
 import HealthPage from "./pages/HealthPage";
@@ -11,10 +10,8 @@ import DegradationPage from "./pages/DegradationPage";
 import ClusteringPage from "./pages/ClusteringPage";
 import ReliabilityPage from "./pages/ReliabilityPage";
 import InsightsPage from "./pages/InsightsPage";
-import RULPage from "./pages/RULPage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
-import LoginPage from "./pages/LoginPage";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -24,22 +21,17 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <Routes>
-          {/* Public */}
-          <Route path="/login" element={<LoginPage />} />
-
-          {/* Protected */}
-          <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-          <Route path="/upload" element={<ProtectedRoute><UploadPage /></ProtectedRoute>} />
-          <Route path="/health" element={<ProtectedRoute><HealthPage /></ProtectedRoute>} />
-          <Route path="/degradation" element={<ProtectedRoute><DegradationPage /></ProtectedRoute>} />
-          <Route path="/clustering" element={<ProtectedRoute><ClusteringPage /></ProtectedRoute>} />
-          <Route path="/reliability" element={<ProtectedRoute><ReliabilityPage /></ProtectedRoute>} />
-          <Route path="/rul" element={<ProtectedRoute><RULPage /></ProtectedRoute>} />
-          <Route path="/insights" element={<ProtectedRoute><InsightsPage /></ProtectedRoute>} />
-          <Route path="/about" element={<ProtectedRoute><AboutPage /></ProtectedRoute>} />
-          <Route path="/contact" element={<ProtectedRoute><ContactPage /></ProtectedRoute>} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/upload" element={<UploadPage />} />
+          <Route path="/health" element={<HealthPage />} />
+          <Route path="/degradation" element={<DegradationPage />} />
+          <Route path="/clustering" element={<ClusteringPage />} />
+          <Route path="/reliability" element={<ReliabilityPage />} />
+          <Route path="/insights" element={<InsightsPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
